0c46fc5eb44f07735789543c4e4dad084ea33ca6
Raphael Campardou <ralovely@gmail.com>
2013-10-16 23:33:23 +1100

    fix helper generator, it does not remove leading space

--- lib/generators/simple_hashtag/templates/views/hashtags_helper.rb
+++ lib/generators/simple_hashtag/templates/views/hashtags_helper.rb
@@ -2,7 +2,7 @@ module HashtagsHelper
   def linkify_hashtags(hashtaggable_content)
     regex = SimpleHashtag::Hashtag::HASHTAG_REGEX
     hashtagged_content = hashtaggable_content.to_s.gsub(regex) do
-      link_to($1, hashtag_path($2), {class: :hashtag})
+      link_to($&, hashtag_path($2), {class: :hashtag})
     end
     hashtagged_content.html_safe
   end
