7eb6ee5d4cd564714f69ec2e3ad77ac2ab82c3a0
EL IDRISSI Abdenasser <nasser.elidrissi065@gmail.com>
2015-08-01 19:08:36 +0100

    Update hashtag.rb

--- lib/simple_hashtag/hashtag.rb
+++ lib/simple_hashtag/hashtag.rb
@@ -9,7 +9,7 @@ module SimpleHashtag
     # TODO Beef up the regex (ie.:what if content is HTML)
     # this is how Twitter does it:
     # https://github.com/twitter/twitter-text-rb/blob/master/lib/twitter-text/regex.rb
-    HASHTAG_REGEX = /#.*[?&]locale=([^&]+)(&|$)/i
+    HASHTAG_REGEX = /#.[\u0600-\u06FF]/i
 
     def self.find_by_name(name)
       Hashtag.where("lower(name) =?", name.downcase).first
