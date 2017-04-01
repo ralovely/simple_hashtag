b080d00446b0ecafedc441adccc6126cf755e204
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-04-01 20:50:22 +0200

    Update create_hashtaggings_migration.rb

--- lib/generators/simple_hashtag/templates/migrations/create_hashtaggings_migration.rb
+++ lib/generators/simple_hashtag/templates/migrations/create_hashtaggings_migration.rb
@@ -2,8 +2,8 @@
 class CreateSimpleHashtagHashtaggings < ActiveRecord::Migration
   def change
     create_table :simple_hashtag_hashtaggings do |t|
-      t.references :hashtag,      :index => true
-      t.references :hashtaggable, :polymorphic => true
+      t.references :hashtag,      :index => { :name => 'index_hashtaggings_hashtag' }
+      t.references :hashtaggable, :polymorphic => true, :index => { :name => 'index_hashtaggings_hashtaggable' }
     end
     add_index :simple_hashtag_hashtaggings, ["hashtaggable_id", "hashtaggable_type"],
               :name => 'index_hashtaggings_hashtaggable_id_hashtaggable_type'
