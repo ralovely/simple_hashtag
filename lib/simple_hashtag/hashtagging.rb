0054d72a068eeef04b91d6ec14a0838e89bc66c3
Alex <13509363+deikka@users.noreply.github.com>
2019-10-15 00:52:17 +0200

    Adding counter_cache

--- lib/simple_hashtag/hashtagging.rb
+++ lib/simple_hashtag/hashtagging.rb
@@ -2,7 +2,7 @@ module SimpleHashtag
   class Hashtagging < ActiveRecord::Base
     self.table_name = "simple_hashtag_hashtaggings"
 
-    belongs_to :hashtag
+    belongs_to :hashtag, counter_cache: true
     belongs_to :hashtaggable, polymorphic: true
   end
 end
